f25603911248250b0314fadcf35a08fc097cc42f
Ben Merryman <ben.merryman@gmail.com>
2022-10-22 18:05:33 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Initial script objects
-const h2 = document.createElement('<h2>');
+const h2 = document.createElement('h2');
 h2.textContent = "js..My personal site for projects, code, and musings";
 
 document.querySelector('h1').append(h2);
