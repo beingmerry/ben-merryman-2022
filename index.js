4bb7fa724f6fb97b9ae571a3178f3da50c29c44e
Ben Merryman <ben.merryman@gmail.com>
2022-10-22 18:17:05 -0400

    index.js text updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Initial script objects
 const h2 = document.createElement("h2");
-h2.textContent = "js..My personal site for projects, code, and musings";
+h2.textContent = "js..My personal site for projects, test code, and musings";
 document.querySelector('h1').append(h2);
