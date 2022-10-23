8abc5722b6c5e790704389a71dca5773b484ce65
Ben Merryman <ben.merryman@gmail.com>
2022-10-22 20:53:49 -0400

    fixed test.js, bug with not using addEventListener anonymous function call

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 const question26Button = document.getElementById("question-26-button");
-question26Button.addEventListener('click', alert("this worked!"));
+question26Button.addEventListener('click', () => alert("this worked!"));
