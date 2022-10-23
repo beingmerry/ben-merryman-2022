f9930931058b0264f59c11b84acc568ca9e03c92
Ben Merryman <ben.merryman@gmail.com>
2022-10-22 22:15:41 -0400

    test.js for h2

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 const question26Button = document.getElementById("question-26");
 question26Button.addEventListener('click', () => alert("this worked!"));
+const h2 = document.getElementsByTagName("h2");
