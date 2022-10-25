6fcea5bdf6ba1fa25c35bea6abf94575620b36fa
Ben Merryman <ben.merryman@gmail.com>
2022-10-25 12:26:46 -0400

    added fieldOfJavaScript files, updated index.html for Nav Links and Meeup event

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Initial script objects
 const h2 = document.createElement("h2");
-h2.textContent = "js..My personal site for projects, test code, and musings";
+h2.textContent = "js..My personal site for projects, test code, and musings, !! NOTE this h2 is being brought in much larger than HTML h2 !! INVESTIGATE";
 document.querySelector('h1').append(h2);
