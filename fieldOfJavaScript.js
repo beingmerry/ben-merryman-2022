1d29086e3fea02ef1b4fa3e306c4a92f773d440e
Ben Merryman <ben.merryman@gmail.com>
2022-10-25 12:27:24 -0400

    added fieldOfJavaScript files

---
+++ fieldOfJavaScript.js
@@ -0,0 +1,4 @@
+// Initial script objects
+const h2 = document.createElement("h2");
+h2.textContent = "js..My personal site for projects, test code, and musings";
+document.querySelector('h1').append(h2);
