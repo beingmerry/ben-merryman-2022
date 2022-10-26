f0cc52d8673bb097abf54513c0975e0a432fb3a6
Ben Merryman <ben.merryman@gmail.com>
2022-10-25 20:08:58 -0400

    append to after in fieldOfJavaScript.js

--- fieldOfJavaScript.js
+++ fieldOfJavaScript.js
@@ -1,4 +1,4 @@
-// Initial script objects
+explo// Initial script objects
 const h2 = document.createElement("h2");
-h2.textContent = "js..My personal site for projects, test code, and musings";
-document.querySelector('h1').append(h2);
+h2.textContent = "My personal site for projects, test code, and musings";
+document.querySelector('h1').after(h2);
