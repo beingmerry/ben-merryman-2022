0490eaecbdeb0de19ae41aa617f5738671773e84
Ben Merryman <ben.merryman@gmail.com>
2022-10-27 16:29:19 -0400

    added 3d printing site and pictures, updated nav bar and removed css testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Initial script objects
-const h2 = document.createElement("h2");
-h2.textContent = "--INSERTED WITH JAVASCRIPT--";
-document.querySelector('h1').after(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "--INSERTED WITH JAVASCRIPT--";
+// document.querySelector('h1').after(h2);
+console.log("Note: testing! Welcome to the Console from Ben Merryman. Checking index.js linked correctly")
